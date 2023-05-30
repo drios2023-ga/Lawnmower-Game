2a6eba0f7458baedc3af045cda519741b7002622
drios2023-ga <drios111-general@yahoo.com>
2023-05-30 14:06:47 -0500

    game now lets user earn money with teeth

--- lawnmower.js
+++ lawnmower.js
@@ -3,40 +3,42 @@
 let savings = 0;
 let toolBox = [];
 let toolList = "";
+let tool = "";
+let buyTool = "";
 
 toolBox.push({
             tool:'teeth',
             cost:0,
             income:1,
-            inTheBox: 1
+            inTheBox: true
 });
 
 toolBox.push({
             tool:'scissors',
             cost:5,
             income:5,
-            inTheBox: 0
+            inTheBox: false
 });
 
 toolBox.push({
             tool:'used mower',
             cost:25,
             income:50,
-            inTheBox: 0
+            inTheBox: false
 });
 
 toolBox.push({
             tool:'new mower',
             cost:250,
             income:100,
-            inTheBox: 0
+            inTheBox: false
 });
 
 toolBox.push({
             tool:'team of students',
             cost:500,
             income:250,
-            inTheBox: 0
+            inTheBox: false
 });
 
 console.log("--------------------------------------");
@@ -46,29 +48,59 @@ console.log(toolBox);
 //let name = prompt("What is your name? : ");
 function buildToolList(){
   for(i=0;i<5;i++){
-      if (toolBox[i].inTheBox===1){
+      if (toolBox[i].inTheBox===true){
         toolList = toolList + "[" + i + "] " + toolBox[i].tool + "\n"
       }
         
     
   }
-console.log(toolList);
+  console.log(toolList);
 }
 
+function incrementSavings(tool){
+  savings = savings + toolBox[tool].income;
+  console.log("Congrats! You earned " + toolBox[tool].income + 
+              " dollar(s) cutting grass with your " + toolBox[tool].tool + ".\n");
 
-// function main(){
-//   do while (toolBox[4].inTheBox === 0 && savings <1000){
-  
+}
+
+function buyATool(tool){
+  if(toolBox[tool]<4 && toolBox[tool+1].inTheBox===false && savings >= toolBox[tool+1].cost){
+    buyTool = prompt("Would you like to buy a " + toolBox[tool+1].tool + "?: (y/n)\n");
+    if (buyTool = 'y'){
+      savings = savings - toolBox[tool+1].cost;
+      toolBox[tool+1].inTheBox = true;
+    }
+  }
+}
+
+function main(){
+  while (toolBox[4].inTheBox === false && savings <1000){
     
-          
-  
-  
+    //pick a tool to use
+    buildToolList();
+    console.log("You have " + savings + " dollar(s) in your savings.\n")
+    tool = prompt("Here are the tools in your toolbox. Which one would you like to use today?: ");
     
-//   }
-// }    
+    //confirm tool is in the box and increment savings
+    if (toolBox[tool].inTheBox===true){
+      incrementSavings(tool);
+      //clear tool list
+      toolList = "";
+
+      //offer a chance to buy a tool
+      buyATool(tool);
+
+    }
+    else {
+      console.log("Uh oh. You picked a tool that isn't in your toolbox.")
+    }      
+  }
+}    
 
 
-buildToolList();
+main();
+
 
 // console.log(toolBox[0]);
 // console.log(toolBox[0].tool);
